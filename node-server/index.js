6fcc045aae4b7fe160aab33e5f85114fcabdbd7c
Jori Lytter <jori.lytter@gmail.com>
2017-05-27 16:50:39 +0300

    Add initial implementation for reading ruuvitag sensors and returing sensor data as JSON

--- node-server/index.js
+++ node-server/index.js
@@ -1,7 +1,14 @@
 const express = require('express')
+const ruuvitag = require('./ruuvitag/sensors')
 const port = 3101
 
 const app = express()
 app.listen(port)
 
 console.log(`express server running on ${port}`)
+
+ruuvitag.start()
+
+app.get('/', function(req, res) {
+  res.json(ruuvitag.data())
+})
