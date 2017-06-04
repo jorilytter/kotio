9ac4e032a0f7aa6407f24723f91e37114f6c237f
Jori Lytter <jori.lytter@gmail.com>
2017-06-04 20:19:45 +0300

    Add basic functionality for listing tellstick switches and turning on and off individual switches and groups

--- node-server/index.js
+++ node-server/index.js
@@ -1,17 +1,36 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const ruuvitag = require('./ruuvitag/sensors')
 const tellstickSensors = require('./tellstick/sensors')
+const tellstickSwitches = require('./tellstick/switches')
 const port = 3101
 
 const app = express()
+app.use(bodyParser.json())
 app.listen(port)
 
 console.log(`express server running on ${port}`)
 
 ruuvitag.start()
 
-app.get('/', function(req, res) {
+app.get('/ruuvitag', function(req, res) {
   res.json(ruuvitag.data())
 })
 
-tellstickSensors.sensors(sensors => console.log('Sensor data:', sensors))
+app.get('/tellstick/sensors', function(req, res) {
+  tellstickSensors.sensors(sensors => res.json(sensors))
+})
+
+app.get('/tellstick/switches', function(req, res) {
+  tellstickSwitches.list(switches => res.json(switches))
+})
+
+app.post('/tellstick/on', function(req, res) {
+  tellstickSwitches.turnOn(req.body)
+  res.json(null)
+})
+
+app.post('/tellstick/off', function(req, res) {
+  tellstickSwitches.turnOff(req.body)
+  res.json(null)
+})
